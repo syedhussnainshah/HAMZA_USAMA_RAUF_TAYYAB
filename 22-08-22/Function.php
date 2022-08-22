4eb25c35d60a09970c88ed266536c16d9967e7f4
Syed Hussnain Shah <itshussnain787@gmail.com>
2022-08-22 14:26:34 +0500

    update

---
+++ 22-08-22/Function.php
@@ -0,0 +1,14 @@
+<?php
+include_once 'connection.php';
+if (isset($_REQUEST['formsubmit'])) {
+   echo $name = $_REQUEST['username'];
+   echo $email = $_REQUEST['useremail'];
+   echo $pass = $_REQUEST['userpass'];
+   $insert = "INSERT INTO signup (name, email, pass) VALUES('$name','$email','$pass')";
+   $query = mysqli_query($conn, $insert);
+   if ($query) {
+      header('location: index.php');
+   } else {
+      echo "No Insert";
+   }
+}
